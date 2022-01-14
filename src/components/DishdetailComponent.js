425d2cc3cdc3b6ae5054980cd6f5742d03e9e025
Nirmal V G <nirmalvg97@gmail.com>
2022-01-14 11:39:47 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -69,9 +69,11 @@ class DishDetail extends Component {
         const dishComment = this.renderComments(dish.comments);
 
         return (
-            <div className="row">
-                {dishItem}
-                {dishComment}
+            <div className="container">
+                <div className="row">
+                    {dishItem}
+                    {dishComment}
+                </div>
             </div>
         );
     }
