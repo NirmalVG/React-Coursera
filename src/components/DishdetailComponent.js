4a93bd126f6aff2a6ba5f611f0a64e146c575512
Nirmal V G <nirmalvg97@gmail.com>
2022-01-26 13:15:24 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -40,7 +40,12 @@ class CommentForm extends Component {
 
     handleCommentFormSubmit(values) {
         console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.props.addComment(
+            this.props.dishId,
+            values.rating,
+            values.author,
+            values.comment
+        );
     }
 
     toggleCommentFormModal() {
@@ -191,7 +196,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ dish, comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments == null) {
         return <div></div>;
     }
@@ -214,7 +219,7 @@ function RenderComments({ dish, comments }) {
         <div className="col-12 col-md-5 m-1">
             <h4> Comments </h4>
             <ul className="list-unstyled">{cmnts}</ul>
-            <CommentForm dish={dish} comments={comments} />
+            <CommentForm dishId={dishId} addComment={addComment} />
         </div>
     );
 }
@@ -244,7 +249,11 @@ const DishDetail = (props) => {
 
             <div className="row">
                 <RenderDish dish={props.dish} />
-                <RenderComments dish={props.dish} comments={props.comments} />
+                <RenderComments
+                    comments={props.comments}
+                    addComment={props.addComment}
+                    dishId={props.dish.id}
+                />
             </div>
         </div>
     );
